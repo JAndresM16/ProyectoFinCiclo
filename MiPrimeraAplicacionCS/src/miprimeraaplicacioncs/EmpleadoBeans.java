1a827a68e9fab1ab0108e23fcedbda4af9587479
JAndresM16 <jmenoscal2352@utm.edu.ec>
2023-09-04 11:38:06 -0500

    Clase Modificada

--- MiPrimeraAplicacionCS/src/miprimeraaplicacioncs/EmpleadoBeans.java
+++ MiPrimeraAplicacionCS/src/miprimeraaplicacioncs/EmpleadoBeans.java
@@ -118,33 +118,59 @@ public class EmpleadoBeans {
     public int Incremento_Empleado() throws SQLException {
         int inc;
         ResultSet rs;
-        rs = bd.consultaBD("SELECT max(id_empleado) as num FROM empleado;");
-        if (rs.next()) {
-            inc = rs.getInt(1) + 1;
-        } else {
-            inc = 1;
+        rs = bd.consultaBD("SELECT max(id_empleado) as num FROM empleado;");      /*3*/
+               if (rs.next()) {                                                   /*4*/  
+            inc = rs.getInt(1) + 1;                                               /*5*/
+        } else {                                                                  /*6*/
+            inc = 1;                                                              /*7*/
         }
         return inc;
+    /* 
+        3/ TA
+        4/ TC
+        5/ TA+TO
+        7/ TA
+       
+        TP = TA+TC+TA+TO = 2TA+TC+TO
+        TM = TA+TC+TA = 2TA+TC
+        TE = 2TA+TC+TO - 2TA+TC = TO
+    */
+
     }
 
     public void Insertar_Empleado() throws SQLException {
         String cadena = "insert into empleado values('" + Incremento_Empleado() + "','" + getId_cargo()
                 + "','" + getId_horario() + "','" + getCedula() + "','" + getNombres() + "','"
-                + getTelefono() + "','" + getDireccion() + "')";
-        bd.ActualizarBD(cadena);
+                + getTelefono() + "','" + getDireccion() + "')";                                          /*1*/
+        bd.ActualizarBD(cadena);                                                                          /*2*/
+        /* 
+        1/ TA
+        2/ 3TA
+        T= TA+3TA =4TA
+    */
 
     }
 
     public void Actualizar_Empleado() throws SQLException {
         String cadena = "update empleado set id_cargo='" + getId_cargo() + "', id_horario= '" + getId_horario()
                 + "', cedula= '" + getCedula() + "', nombres= '" + getNombres() + "', telefono= '" + getTelefono()
-                + "', direccion= '" + getDireccion() + "' where id_empleado= '" + getId_Empleado() + "'";
-        bd.ActualizarBD(cadena);
+                + "', direccion= '" + getDireccion() + "' where id_empleado= '" + getId_Empleado() + "'";              /*1*/
+        bd.ActualizarBD(cadena);                                                                                       /*2*/
+        /* 
+        1/ TA
+        2/ 3TA
+        T= TA+3TA =4TA
+    */
     }
 
     public void Eliminar_Empleado() throws SQLException {
-        String cadena = "delete from empleado where id_empleado='" + getId_Empleado() + "'";
-        bd.ActualizarBD(cadena);
+        String cadena = "delete from empleado where id_empleado='" + getId_Empleado() + "'";   /*1*/
+        bd.ActualizarBD(cadena);                                                               /*2*/
+        /* 
+        1/ TA
+        2/ 2TA
+        T= TA+2TA =3TA
+    */
     }
 
     public ResultSet consultaTabla(String sql) throws SQLException {
@@ -153,8 +179,11 @@ public class EmpleadoBeans {
 
     public void Consultar_Empleado() throws SQLException {
         ResultSet rs;
-        rs = bd.consultaBD("SELECT e.cedula, e.nombres, c.cargo, h.horaInicio, h.horaFin, c.sueldo FROM empleado e JOIN cargo c ON e.id_cargo = c.id_cargo JOIN horario h ON e.id_horario = h.id_horario");
-        while (rs.next()) {
+        rs = bd.consultaBD("SELECT e.cedula, e.nombres, c.cargo, h.horaInicio, "
+                + "h.horaFin, c.sueldo FROM empleado e "
+                + "JOIN cargo c ON e.id_cargo = c.id_cargo JOIN horario "
+                + "h ON e.id_horario = h.id_horario");                   /*1*/ 
+        while (rs.next()) {                                              /*2*/
             System.out.print(rs.getString("cedula") + " ");
             System.out.print(rs.getString("nombres") + " ");
             System.out.print(rs.getString("cargo") + " ");
@@ -163,6 +192,13 @@ public class EmpleadoBeans {
             System.out.print(rs.getDouble("sueldo") + " ");
             System.out.println("");
         }
+    /* 
+        1/ 2TA
+        2/ N*TC+TC
+        TP= 2TA+N*TC+TC
+        TM= 2TA+TC
+        TE= 2TA+N*TC+TC - 2TA+TC = N*TC
+    */
     }
 
     public ResultSet consultarIDs() throws SQLException {
